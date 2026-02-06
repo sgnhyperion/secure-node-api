ecd78e5f29901e3e2da0cd6f9b43d70de42837de
Harsh <hk755063@gmail.com>
2026-02-06 18:34:19 +0530

    minor change

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json("Welcome to the secure node api v2");
+    res.json("Welcome to the secure node api v3");
 }); 
 
 
